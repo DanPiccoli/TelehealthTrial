8ac525455794e179cbddb9e55f31601654e9eadc
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-23 11:10:45 -0500

    Accidentally deleted 'delete' as well

--- client/src/App/pages/components/exercises-list.component.js
+++ client/src/App/pages/components/exercises-list.component.js
@@ -8,7 +8,9 @@ const Exercise = props => (
     <td>{props.exercise.description}</td>
     <td>{props.exercise.duration}</td>
     <td>{props.exercise.date.substring(0,10)}</td>
-    
+    <td>
+      <Link to={ <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+    </td>
   </tr>
 )
 
