59aa3c2ea596fafe19e2f45454b241db1ff5e294
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-23 11:14:12 -0500

    Match tags

--- client/src/App/pages/components/exercises-list.component.js
+++ client/src/App/pages/components/exercises-list.component.js
@@ -9,7 +9,7 @@ const Exercise = props => (
     <td>{props.exercise.duration}</td>
     <td>{props.exercise.date.substring(0,10)}</td>
     <td>
-      <Link to={ <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+      <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
     </td>
   </tr>
 )
